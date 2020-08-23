776e1a586916f6b9581c3381170f466fe2caecc4
Hector R <github.hector1@gmail.com>
2020-08-22 21:31:01 -0400

    Implemented Structures for the Patch Messges (protocol 8)

---
+++ messages/misc/patch_msg.go
@@ -0,0 +1,52 @@
+package misc
+
+/*
+File contains Structure definitions for Patch protocol (8).
+*/
+
+/*
+Patch Client sends this message to request the latest file list.
+Patch Server sends this message to tell the client where to get it.
+*/
+type LatestFileList struct {
+	LatestVersion uint32
+	ListFileName  string
+	ListFileType  uint32
+	ListFileTime  uint32
+	ListFileSize  uint32
+	ListFileCRC   uint32
+	ListFileURL   string
+	URLPrefix     string
+	URLSuffix     string
+}
+
+/*
+Patch Client sends this message to request the latest file list.
+Patch Server sends this message to tell the client where to get it.
+Same as above but with locale
+*/
+type LatestFileListV2 struct {
+	LatestVersion uint32
+	ListFileName  string
+	ListFileType  uint32
+	ListFileTime  uint32
+	ListFileSize  uint32
+	ListFileCRC   uint32
+	ListFileURL   string
+	URLPrefix     string
+	URLSuffix     string
+	Locale        string
+}
+
+/*
+Patch Client sends this message with the current version of a specific package.
+Patch Server replies with the next version of the specific package.
+*/
+
+type NextVersion struct {
+	PkgName   string
+	Version   int32
+	URLPrefix string
+	Filename  string
+	FileType  int32
+}
